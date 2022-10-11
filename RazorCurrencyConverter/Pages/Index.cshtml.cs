e5b0b0c9f63a8d0d31e75cf307f3929454408f65
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2022-10-11 10:04:13 +0100

    Add project files.

---
+++ RazorCurrencyConverter/Pages/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+
+namespace RazorCurrencyConverter.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+
+        public string FullName { get; set; }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            FullName = "Sudath Namwagamuwage";
+        }
+
+        public void OnPost()
+        {   
+            
+            if(String.IsNullOrWhiteSpace(FullName))
+            {
+                ViewData["Message"] = "Name is missing!";
+                FullName = "Anonymous";
+            }
+            else
+            {
+                ViewData["Message"] = "Name is Registerd!";
+                //Register the user
+            }
+
+        }
+
+    }
+}
